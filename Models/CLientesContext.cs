f0d0de91f78c82ba613f3a27fdca4f1dc77ac32e
Andres <37418332+kustom89@users.noreply.github.com>
2025-04-14 23:55:26 -0400

    commit

---
+++ Models/CLientesContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClienteApi.Models;
+
+public partial class CLientesContext : DbContext
+{
+    public CLientesContext()
+    {
+    }
+
+    public CLientesContext(DbContextOptions<CLientesContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cliente> Clientes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cliente>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Clientes__3214EC07A1B66947");
+
+            entity.Property(e => e.Nacionalidad).HasMaxLength(50);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+            entity.Property(e => e.Telefono).HasMaxLength(20);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClienteApi.Models;
+
+public partial class Cliente
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public int? Edad { get; set; }
+
+    public string? Telefono { get; set; }
+
+    public string? Nacionalidad { get; set; }
+}
